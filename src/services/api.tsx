3209fe08e5e45a27f20175ed4559b15b53532506
Victor Feliciano <victorjfeliciano@gmail.com>
2023-06-08 13:47:22 -0300

    add private request

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import { getToken } from '../utils/autentication';
 
-export default axios.create({
+export const api = axios.create({
     baseURL: 'https://api-contacts.pedagogico.cubos.academy',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
 });
+
+export async function privatePostRequest(path: string, body: object) {
+    const token = getToken();
+
+    try {
+
+        const response = api.post(path, body, {
+            headers: { 'Authorization': `Bearer ${token}` }
+        })
+        return response
+
+    } catch (error) {
+        return error
+    }
+}
